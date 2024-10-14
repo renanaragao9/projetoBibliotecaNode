be959765a5041d12c169906fbaf9bbfa1924d7e2
renanaragao9 <renanaragao159@gmail.com>
2024-10-14 18:28:55 -0300

    Finalizando projeto

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express              from "express";
 import conectaNaDatabase    from "../src/config/dbConnect.js";
-import livro                from "../src/models/Livro.js"
+import routes               from "./routes/index.js";
 
 const conexao = await conectaNaDatabase();
 
@@ -13,28 +13,7 @@ conexao.once("open", () => {
 });
 
 const app = express();
-app.use(express.json());
-
-
-app.get("/", (req, res) => {
-    res.status(200).send("Curso de node.js");
-});
-
-app.get("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index]);
-});
-
-app.post("/livros", (req, res) => {
-    livros.push(req.body);
-    res.status(201).send("Livro cadastrado com sucesso!");
-});
-
-app.put("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.status(200).json(livros);
-});
+routes(app);
 
 app.delete("/livros/:id", (req, res) => {
     const index = buscaLivro(req.params.id);
